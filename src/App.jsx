d7de2b2a9379e6418e15e79d209ef71dddc84df4
Shashank <pshashank1729@gmail.com>
2023-04-18 17:01:51 +0530

    apps completed colorPicker Calendar Kanban Editor

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
                 {/* App's routes */}
                 <Route path="/kanban" element={<Kanban />} />
                 <Route path="/editor" element={<Editor />} />
-                <Route path="/calender" element={<Calendar />} />
+                <Route path="/calendar" element={<Calendar />} />
                 <Route path="/color-picker" element={<ColorPicker />} />
 
                 {/* charts */}
